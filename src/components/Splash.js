14efb9d7d11087fc0589a4a367061e1592bf8016
isaias salgado <isalgado_21@alu.uabcs.mx>
2024-06-02 22:01:31 -0700

    creamos una interaz y limpiamos los archivos por defecto de react

---
+++ src/components/Splash.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Splash.css';
+
+const Splash = () => {
+  return (
+    <div className="splash-container">
+      <img src="/logo.png" alt="Veterinary Logo" className="splash-logo" />
+      <h1>Welcome to Our Veterinary Clinic</h1>
+    </div>
+  );
+};
+
+export default Splash;
