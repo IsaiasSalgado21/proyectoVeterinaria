b71b2873edf79dd6fae05637a7f98a486b656fb5
job moore <jobmurdan@ohtmail.com>
2024-06-12 18:21:17 -0700

    he creado el funcionamiento de la splash screen

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,8 +1,19 @@
-// src/components/Splash.js
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Splash.css';
 
 const Splash = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/home');
+    }, 5000); // 5 segundos
+
+    // Limpia el temporizador si el componente se desmonta antes de que el temporizador se complete
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
   return (
     <div className="splash-container">
       <img src="/logo.png" alt="Veterinary Logo" className="splash-logo" />
